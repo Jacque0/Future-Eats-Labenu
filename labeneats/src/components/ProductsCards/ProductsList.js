f319583814766f32461070aaec6c5653cd03706a
Jacqueline Nascimento <jacqueline.cmessias@gmail.com>
2022-03-18 01:18:38 -0300

    desfazendo alterações no componente productlist

--- labeneats/src/components/ProductsCards/ProductsList.js
+++ labeneats/src/components/ProductsCards/ProductsList.js
@@ -8,7 +8,6 @@ import {
 import Button from "@mui/material/Button";
 import React from "react";
 import theme from "../../constants/theme";
-import ProductCard from "../ProductCard/ProductCard";
 import { TextContainer, CategoryCard, Hr } from "./styledProductsList";
 
 function ProductsList(props) {
@@ -38,87 +37,86 @@ function ProductsList(props) {
 
         {item.map((item) => {
           return (
-            <ProductCard product={item} />
-            // <Card
-            //   sx={{
-            //     maxWidth: 345,
-            //     borderRadius: 3,
-            //     margin: "auto",
-            //     marginBottom: 5,
-            //   }}
-            //   data={item}
-            //   key={item.id}
-            //   category={item.category}
-            // >
-            //   <CardActionArea sx={{ display: "flex", height: "112px" }}>
-            //     <CardMedia
-            //       component="img"
-            //       image={item.photoUrl}
-            //       height="150"
-            //       alt="Prato"
-            //       sx={{ width: "96px" }}
-            //     />
-            //     <CardContent sx={{ width: "80%" }}>
-            //       <Typography
-            //         sx={{
-            //           width: "75%",
-            //           lineHeight: "12px",
-            //         }}
-            //         gutterBottom
-            //         variant="subtitle2"
-            //         component="div"
-            //         color="primary.main"
-            //       >
-            //         {item.name}
-            //       </Typography>
-            //       <TextContainer>
-            //         <Typography
-            //           variant="body2"
-            //           sx={{ color: theme.palette.terciary.main }}
-            //         >
-            //           {item.description}
-            //         </Typography>
-            //         <Typography
-            //           variant="h6"
-            //           sx={{ color: "black", marginTop: "1px" }}
-            //         >
-            //           R${parseInt(item.price).toFixed(2)}
-            //         </Typography>
-            //       </TextContainer>
-            //     </CardContent>
-            //     <Button
-            //       sx={{
-            //         borderRadius: "0px 8px 0px 8px",
-            //         height: "31px",
-            //         border: "1px solid #5cb646",
-            //         fontSize: "small",
-            //         color: "primary.main",
-            //         alignSelf: "flex-start",
-            //         position: "absolute",
-            //         left: "272px",
-            //       }}
-            //       size="small"
-            //     >
-            //       1
-            //     </Button>
-            //     <Button
-            //       sx={{
-            //         borderRadius: "8px 0px 8px 0px",
-            //         width: "90px",
-            //         height: "31px",
-            //         border: "1px solid #5cb646",
-            //         fontSize: "small",
-            //         color: "primary.main",
+            <Card
+              sx={{
+                maxWidth: 345,
+                borderRadius: 3,
+                margin: "auto",
+                marginBottom: 5,
+              }}
+              data={item}
+              key={item.id}
+              category={item.category}
+            >
+              <CardActionArea sx={{ display: "flex", height: "112px" }}>
+                <CardMedia
+                  component="img"
+                  image={item.photoUrl}
+                  height="150"
+                  alt="Prato"
+                  sx={{ width: "96px" }}
+                />
+                <CardContent sx={{ width: "80%" }}>
+                  <Typography
+                    sx={{
+                      width: "75%",
+                      lineHeight: "12px",
+                    }}
+                    gutterBottom
+                    variant="subtitle2"
+                    component="div"
+                    color="primary.main"
+                  >
+                    {item.name}
+                  </Typography>
+                  <TextContainer>
+                    <Typography
+                      variant="body2"
+                      sx={{ color: theme.palette.terciary.main }}
+                    >
+                      {item.description}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      sx={{ color: "black", marginTop: "1px" }}
+                    >
+                      R${parseInt(item.price).toFixed(2)}
+                    </Typography>
+                  </TextContainer>
+                </CardContent>
+                <Button
+                  sx={{
+                    borderRadius: "0px 8px 0px 8px",
+                    height: "31px",
+                    border: "1px solid #5cb646",
+                    fontSize: "small",
+                    color: "primary.main",
+                    alignSelf: "flex-start",
+                    position: "absolute",
+                    left: "272px",
+                  }}
+                  size="small"
+                >
+                  1
+                </Button>
+                <Button
+                  sx={{
+                    borderRadius: "8px 0px 8px 0px",
+                    width: "90px",
+                    height: "31px",
+                    border: "1px solid #5cb646",
+                    fontSize: "small",
+                    color: "primary.main",
 
-            //         position: "absolute",
-            //         left: "247px",
-            //         top: "82px",
-            //       }}
-            //     >
-            //       adicionar
-            //     </Button>
-            //   </CardActionArea>
-            // </Card>
+                    position: "absolute",
+                    left: "247px",
+                    top: "82px",
+                  }}
+                >
+                  adicionar
+                </Button>
+              </CardActionArea>
+            </Card>
           );
         })}
       </CategoryCard>

4235956ec1882bfc72ffd9f6ee487d68c0eb03d6
Leo Rigotto <leorigotto@me.com>
2022-03-18 16:07:31 -0300

    warning funcional

--- labeneats/src/components/OrderWarning/OrderWarning.js
+++ labeneats/src/components/OrderWarning/OrderWarning.js
@@ -2,20 +2,24 @@ import React from "react"
 import { BASE_URL } from "../../constants/BASE_URL"
 import { useRequestData } from "../../hooks/useRequestData"
 import { Container, OrderContainer } from "./styledOrderWarning"
+import AccessTimeIcon from "@mui/icons-material/AccessTime"
 
 export default function OrderWarning() {
+  const { data, error, loading } = useRequestData(
+    { order: {} },
+    BASE_URL + "/active-order"
+  )
 
-    const { data, error, loading } = useRequestData({order: {}}, BASE_URL+ '/active-order')
-
-    console.log(data)
-
-  return (
+  const renderWarning = (
     <Container>
-        <OrderContainer>
-      <p className="orderTitle">Pedido em andamento</p>
-      <p className="orderRestaurant"></p>
-      <p className="orderPrice"></p>
-        </OrderContainer>
+      <AccessTimeIcon className="icon" />
+      <OrderContainer>
+        <p className="orderTitle">Pedido em andamento</p>
+        <p className="orderRestaurant">{data?.order.restaurantName}</p>
+        <p className="orderPrice">TOTAL R${data?.order.totalPrice}</p>
+      </OrderContainer>
     </Container>
   )
+
+  return <>{data.order.restaurantName ? renderWarning : <></>}</>
 }
--- labeneats/src/components/OrderWarning/styledOrderWarning.js
+++ labeneats/src/components/OrderWarning/styledOrderWarning.js
@@ -5,17 +5,35 @@ export const Container = styled.div`
   align-items: center;
   width: 100%;
   position: fixed;
+  z-index: 2;
   bottom: 49px;
-  padding: 24px;
+  padding: 12px 24px;
   background-color: #5cb646;
+  .icon {
+    color: white;
+    margin-right: 24px;
+    transform: scale(1.3);
+  }
+  animation: appear 500ms ease forwards;
+  @keyframes appear {
+    from {
+      bottom: -49px;
+    }
+    to {
+      bottom: 49px;
+    }
+  }
 `
 
 export const OrderContainer = styled.div`
   display: flex;
   flex-direction: column;
+  p {
+    margin-bottom: 3px;
+  }
   .orderTitle {
     font-size: 16px;
-    font-weight: normal;
+    font-weight: lighter;
     font-stretch: normal;
     font-style: normal;
     line-height: normal;
